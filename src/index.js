362321cdbb839d06a8a6ab24f92fb5bde0aa49b8
Yilchik <yulyayulia2@gmail.com>
2024-07-23 20:33:41 +0200

    getAllContacts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-import * as fs from 'node:fs/promises';
 import { PATH_DB } from './constants/contacts';
-
-fs.readFile(PATH_DB, { encoding: 'UTF-8' })
-  .then((data) => JSON.parse(data))
-  .catch((error) => console.error(error));
--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,5 +1,14 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
 
-export const getAllContacts = async () => {};
+export const getAllContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, { encoding: 'UTF-8' });
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 console.log(await getAllContacts());
